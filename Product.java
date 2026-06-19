3c55cb6c81f81f63e6e4851451516411a06130ab
Vishwaathiga <vishwaamithiran@gmail.com>
2026-06-19 13:06:47 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,12 @@
+class Product {
+
+    int productId;
+    String productName;
+    String category;
+
+    Product(int id, String name, String category) {
+        this.productId = id;
+        this.productName = name;
+        this.category = category;
+    }
+}
