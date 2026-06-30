0765793b6fdf45f5259d02e8da9e1c837e8adb93
Vishwaathiga <vishwaamithiran@gmail.com>
2026-06-30 13:20:13 +0530

    Update BookService.java
    Removed comments in Tamil and simplified the code.

--- Week2/Spring_Core_Maven/Exercise1_2_4_LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week2/Spring_Core_Maven/Exercise1_2_4_LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -4,21 +4,16 @@ import com.library.repository.BookRepository;
 
 public class BookService {
 
-    // Dependency — BookRepository வேணும் BookService க்கு
     private BookRepository bookRepository;
-
     public BookService() {
         System.out.println("BookService object created!");
     }
-
-    // Setter Injection — Exercise 2 க்கு இதுதான் key!
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
         System.out.println("BookRepository injected into BookService!");
     }
 
     public void displayBookInfo() {
-        // BookRepository-ஐ பயன்படுத்துறோம்
         System.out.println(bookRepository.getBookDetails());
     }
 }
