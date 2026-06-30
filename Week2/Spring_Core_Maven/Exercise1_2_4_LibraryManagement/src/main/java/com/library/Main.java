9deffafcccdfb0fd96406559cad502571340928b
Vishwaathiga <vishwaamithiran@gmail.com>
2026-06-30 13:21:12 +0530

    Update Main.java

--- Week2/Spring_Core_Maven/Exercise1_2_4_LibraryManagement/src/main/java/com/library/Main.java
+++ Week2/Spring_Core_Maven/Exercise1_2_4_LibraryManagement/src/main/java/com/library/Main.java
@@ -5,17 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.library.service.BookService;
 
 public class Main {
-    public static void main(String[] args) {
-
-        // Spring IoC Container load பண்றோம் — XML file படிக்கும்!
+    public static void main(String[] args) { 
         ApplicationContext context = 
             new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        // Spring-ஓட "bookService" bean-ஐ எடுக்கறோம்
-        // (BookRepository ஏற்கனவே automatic ஆ inject ஆகியிருக்கும்!)
-        BookService bookService = (BookService) context.getBean("bookService");
-
-        // Method call பண்ணு
+        BookService bookService = (BookService) context.getBean("bookService");ு
         bookService.displayBookInfo();
     }
 }
